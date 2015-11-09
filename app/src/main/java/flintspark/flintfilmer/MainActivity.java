3be30a0cdb2dc329f8916e35949728d7ec2267a2
Traitortots <theholographicgamer@gmail.com>
2015-11-09 13:53:09 -0700

    added project to repository

---
+++ app/src/main/java/flintspark/flintfilmer/MainActivity.java
@@ -0,0 +1,55 @@
+package flintspark.flintfilmer;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.hardware.camera2.CameraManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CameraManager camera;
+    private String[] cameraIdList;
+    private Toast toast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        camera = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try
+        {
+            cameraIdList = camera.getCameraIdList();
+        }
+        catch(CameraAccessException e)
+        {
+            toast.makeText(getApplicationContext(), "Error Cant Open Camera Id List", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    /**
+     * Looks for a camera and returns if it has one or not;
+     * @return cameraDetected
+     */
+    private boolean detectCamera()
+    {
+        boolean cameraDetected = false;
+
+//        if(camera.get() > 0)
+        {
+
+        }
+//        else
+        {
+
+        }
+
+
+        return cameraDetected;
+    }
+
+}
